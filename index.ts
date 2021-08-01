fc9ae71ead10013c042fdb5d3df805bfd52f8d26
Allal Mohamed Lamine <allalmohmaedlamine@gmail.com>
2021-08-01 01:42:08 +0100

    update: add instance.Model.tableName test

--- index.ts
+++ index.ts
@@ -3,34 +3,44 @@
  */
 
 import { Model } from 'sequelize/types';
-import db from './db';
 import { Course } from './db/models/Course';
 import { User } from './db/models/User';
 
-function runTest(instance: Model) {
+function runInstanceTest(instance: Model, instanceName: string) {
   console.log('\nInstance access using instance.name and instance.tableName')
 
   console.log(
-    'instance.name: ' + (instance as any).name
+    `${instanceName}.name: ${(instance as any).name}`
   )
   console.log(
-    'instance.tableName: ' + (instance as any).tableName
+    `${instanceName}.tableName: ${(instance as any).tableName}`
   )
-
+  
   console.log('\nInstance access using instance.constructor.name and instance.constructor.tableName\n')
+  
+  console.log(
+    `${instanceName}.constructor.name: ${instance.constructor.name}`
+  )
+  console.log(
+    `${instanceName}.constructor.tableName: ${(instance.constructor as any).tableName}`
+  )
 
-  console.log(
-    'instance.constructor.name: ' + instance.constructor.name
-  )
-  console.log(
-    'instance.constructor.tableName: ' + (instance.constructor as any).tableName
-  )
+  // console.log('\nInstance access using instance.Model.name and instance.Model.tableName\n')
+
+  // console.log(
+  //   `${instanceName}.Model.name: ${(instance as any).Model.name}`
+  // )
+  // console.log(
+  //   `${instanceName}.Model.tableName: ${(instance as any).Model.tableName}`
+  // )
 }
 
 (async () => {
   // await db.sync()
 
   // ______________________ Models Classes ___________________
+  console.log('--------------- Testing Models instances -----------------------\n')
+
   console.log('\n\nStatic access using getTableName() (without Schema) :\n');
 
   console.log('Course.getTableName(): ' + Course.getTableName());
@@ -47,20 +57,21 @@ function runTest(instance: Model) {
   console.log('User.name: ' + (User as any).name);
   console.log('User.tableName: ' + (User as any).tableName);
   
+  console.log('\n----\n')
 
   // ______________ Models instances _________________________
 
-  console.log('\n----\n')
+  console.log('---------------------- Testing Models Classes ----------------\n')
 
   const course = Course.build({ name: 'Sequelize model name and tableName', description: 'Show clearly how to do it and the different options' });
 
   const user = User.build({ name: 'Mohamed Lamine Allal', email: 'allalmohamedlamine@gmail.com' });
 
   console.log('\nTesting using  Class Extends Model definition :::: \n')
-  runTest(course);
+  runInstanceTest(course, 'course');
 
   console.log('\n\nTesting using  sequelize.define() definition :::: \n')
-  runTest(user);
+  runInstanceTest(user, 'user');
 })()
 
 
